d01b35db4dc419282a5da8f2f93aaf213af8f944
Ihor_M <igorro_1@ukr.net>
2024-10-07 19:26:51 +0300

    hw_7

---
+++ homework7.js
@@ -0,0 +1,10 @@
+
+function hidePhoneNumbers(text) {
+    // напишіть ваш код тут
+  
+    return text.replace(pattern, "XXX-XXX-XXXX");
+  }
+
+
+
+   //console.log(/\ba/.exec("jhjhj sdjhj shdjhd sjhdjsdj"));
